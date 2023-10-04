6741575495e28af6dc7dfc8f744e3188966ae144
Daniil Mikrukov <elmikrukov@outlook.com>
2023-10-04 12:57:17 +0400

    init project

---
+++ src/test/java/com/edu/SpringLr/SpringLrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.SpringLr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
